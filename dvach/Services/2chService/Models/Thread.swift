5d3d5f83480e7d6678013fed890ac67cc7a55c3d
Ruslan Timchenko <rustimchek@mail.ru>
2019-05-31 18:58:10 +0300

    Создал модель для парсинга Thread

--- dvach/Services/2chService/Models/Thread.swift
+++ dvach/Services/2chService/Models/Thread.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+struct Thread {
+    let comment: String
+    let lastHit: Int // Как я понял, это таймстамп последней активности в треде
+    let num: String
+    let postsCount: Int
+    let score: Double?
+    let subject: String
+    let timestamp: Int // А вот это должно быть время создания треда
+    let views: Int?
+    let additionalInfo: ThreadAdditionalInfo?
+}
+
+// MARK: - JSONParsable
+
+extension Thread: JSONParsable {
+    static func from(json: JSON) -> Thread? {
+        guard let comment = json["comment"].string,
+            let lastHit = json["lasthit"].int,
+            let num = json["num"].string,
+            let postsCount = json["posts_count"].int,
+            let subject = json["subject"].string,
+            let timestamp = json["timestamp"].int else { return nil }
+        
+        let score = json["score"].double
+        let views = json["views"].int
+        let additionalInfo = ThreadAdditionalInfo.from(json: json)
+        
+        return Thread(comment: comment,
+                      lastHit: lastHit,
+                      num: num,
+                      postsCount: postsCount,
+                      score: score,
+                      subject: subject,
+                      timestamp: timestamp,
+                      views: views,
+                      additionalInfo: additionalInfo)
+    }
+}
