0200011c58c20d0d7505c898107c4134c760621c
Ruslan Timchenko <rustimchek@mail.ru>
2019-06-06 15:38:58 +0300

    Создал модель для парсинга Post

---
+++ dvach/Services/2chService/Models/Post.swift
@@ -0,0 +1,93 @@
+//
+//  Post.swift
+//  dvach
+//
+//  Created by Ruslan Timchenko on 06/06/2019.
+//  Copyright © 2019 Kirill Solovyov. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct Post {
+    let isBanned: Bool
+    let isClosed: Bool
+    let comment: String
+    let date: String
+    let email: String
+    let isEndless: Bool
+    let files: [File]
+    let lastHit: Int
+    let name: String
+    let num: String
+    let isOp: Bool
+    let parent: String
+    let sticky: Int
+    let subject: String
+    let timestamp: Int
+    let trip: String
+    let tripType: String?
+    let likes: Int?
+    let dislikes: Int?
+    let filesCount: Int?
+    let postsCount: Int?
+    let tags: String?
+    let uniquePosters: String? // В API видел число 110 один раз в строке
+}
+
+// MARK: - JSONParsable
+
+extension Post: JSONParsable {
+    static func from(json: JSON) -> Post? {
+        guard let isBanned = json["banned"].int,
+            let isClosed = json["closed"].int,
+            let comment = json["comment"].string,
+            let date = json["date"].string,
+            let email = json["email"].string,
+            let isEndless = json["endless"].int,
+            let filesArray = json["files"].array,
+            let lastHit = json["lasthit"].int,
+            let name = json["name"].string,
+            let num = json["num"].string,
+            let isOp = json["op"].int,
+            let parent = json["parent"].string,
+            let sticky = json["sticky"].int,
+            let subject = json["subject"].string,
+            let timestamp = json["timestamp"].int,
+            let trip = json["trip"].string else { return nil }
+        
+        let tripType = json["trip_type"].string
+        let likes = json["likes"].int
+        let dislikes = json["dislikes"].int
+        let filesCount = json["files_count"].int
+        let postsCount = json["posts_count"].int
+        let tags = json["tags"].string
+        let uniquePosters = json["unique_posters"].string
+        
+        let files = filesArray.compactMap(File.from)
+        
+        return Post(isBanned: isBanned.boolValue,
+                    isClosed: isClosed.boolValue,
+                    comment: comment,
+                    date: date,
+                    email: email,
+                    isEndless: isEndless.boolValue,
+                    files: files,
+                    lastHit: lastHit,
+                    name: name,
+                    num: num,
+                    isOp: isOp.boolValue,
+                    parent: parent,
+                    sticky: sticky,
+                    subject: subject,
+                    timestamp: timestamp,
+                    trip: trip,
+                    tripType: tripType,
+                    likes: likes,
+                    dislikes: dislikes,
+                    filesCount: filesCount,
+                    postsCount: postsCount,
+                    tags: tags,
+                    uniquePosters: uniquePosters)
+    }
+}
