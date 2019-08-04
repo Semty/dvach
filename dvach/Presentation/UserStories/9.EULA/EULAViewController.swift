8ebc5a0616505bd652f82ac5d799f5991186bffd
Ruslan Timchenko <rustimchek@mail.ru>
2019-08-04 17:52:16 +0300

    Добавил текст eula

--- dvach/Presentation/UserStories/9.EULA/EULAViewController.swift
+++ dvach/Presentation/UserStories/9.EULA/EULAViewController.swift
@@ -10,23 +10,112 @@ import Foundation
 
 private extension String {
     static let eula = """
-Definitions
-    
-    In this EULA the expressions below shall have the meaning assigned to them in this clause, unless the context requires otherwise:
-    
-    “Activate” turning the Trial version into the Full version of the same application with the License Key provided by Sketch;
-    “Contributor” You when You are assigned the role of contributor via the Sketch collaboration tool Sketch for Teams;
-    “Contributor version” the license to Use the Sketch Mac App for the term that You qualify as a Contributor;
-    “Documentation” the detailed information about the Sketch Mac App, its features and the system requirements as made available on the website of Sketch, as amended from time to time;
-    “Full version” the license for the Sketch Mac App for the term specified on the webpage of the store where You purchase the license, or in any applicable agreement concerning the purchase of the license (as stand-alone product or as part of a subscription) to Use the Sketch Mac App;
-    “License Key” an unique code provided by Sketch, which enables You to activate the Trial version, Full version or Contributor version by entering the code into the Sketch Mac App and to subsequently use the Sketch Mac App during the applicable license term;
-    “Open Source Software” any software that requires as a condition of use, copying, modification and/or distribution of such software that such software or other software incorporated into, derived from or distributed with such software (a) be disclosed or distributed in source code form, and (b) be licensed for the purpose of making and/or distributing derivative works, and (c) be redistributable at no charge;
-    “Sketch” Sketch B.V., Flight Forum 40, Ground floor, 5657 DB Eindhoven, the Netherlands with chamber of commerce registration number 60360461 and VAT number NL8538.75.273.B01;
-    “Sketch Mac App” any software application and/or all of the contents of the files and/or other media, including software setup files, licensed to You by Sketch, including any Updates;
-    “Trial version” the license for the Sketch Mac App for the term of 30 days to Use the Sketch Mac App for the sole purpose of testing and evaluating the Sketch Mac App;
-    “Updates” any modified versions and updates of, and additions to the Sketch Mac App (excluding upgrades of the Sketch Mac App);
-    “Use” the access, download, install, copy or get benefit from using the Sketch Mac App in accordance with the documentation;
-    “You” you, the final and ultimate user of the Sketch Mac App or the authorized representative of a company or other legal entity that will be the final and ultimate user of the Sketch Mac App, and the company or other legal entity that will be the final and ultimate user of the Sketch Mac App, if applicable.
+Двач/2ch - анонимный браузер
+
+Copyright (c) 2019 Ruslan Timchenko and Kirill Solovyov
+
+*** END USER LICENSE AGREEMENT ***
+
+IMPORTANT: PLEASE READ THIS LICENSE CAREFULLY BEFORE USING THIS SOFTWARE.
+
+1. LICENSE
+
+By receiving, opening the file package, and/or using Двач/2ch - анонимный браузер containing this software, you agree that this End User User License Agreement (EULA) is a legally binding and valid contract and agree to be bound by it. You agree to abide by the intellectual property laws and all of the terms and conditions of this Agreement.
+
+Unless you have a different license agreement signed by Ruslan Timchenko and Kirill Solovyov your use of Двач/2ch - анонимный браузер indicates your acceptance of this license agreement and warranty.
+
+Subject to the terms of this Agreement, Ruslan Timchenko and Kirill Solovyov grants to you a limited, non-exclusive, non-transferable license, without right to sub-license, to use Двач/2ch - анонимный браузер in accordance with this Agreement and any other written agreement with Ruslan Timchenko and Kirill Solovyov. Ruslan Timchenko and Kirill Solovyov does not transfer the title of Двач/2ch - анонимный браузер to you; the license granted to you is not a sale. This agreement is a binding legal agreement between Ruslan Timchenko and Kirill Solovyov and the purchasers or users of Двач/2ch - анонимный браузер.
+
+If you do not agree to be bound by this agreement, remove Двач/2ch - анонимный браузер from your device now and, if applicable, promptly return to Ruslan Timchenko and Kirill Solovyov by mail any copies of Двач/2ch - анонимный браузер and related documentation and packaging in your possession.
+
+2. YOUR ACCESS TO THE SERVICES
+
+Children under the age of 13 are not allowed to use the Services. Additionally, if you are in the European Economic Area, you must be over the age required by the laws of your country to use the Services, or we need to have received verifiable consent from your parent or legal guardian.
+
+In addition, certain of our Services or portions of our Services require you to be older than 13 years of age, so please read all notices and any Additional Terms carefully when you access the Services.
+
+If you are accepting these Terms on behalf of another legal entity, including a business or a government, you represent that you have full legal authority to bind such entity to these terms.
+
+3. DISTRIBUTION
+
+Двач/2ch - анонимный браузер and the license herein granted shall not be copied, shared, distributed, re-sold, offered for re-sale, transferred or sub-licensed in whole or in part except that you may make one copy for archive purposes only. For information about redistribution of Двач/2ch - анонимный браузер contact Ruslan Timchenko and Kirill Solovyov.
+
+4. USER AGREEMENT
+
+4.1 Use
+
+Your license to use Двач/2ch - анонимный браузер is limited to the number of licenses purchased by you. You shall not allow others to use, copy or evaluate copies of Двач/2ch - анонимный браузер.
+
+4.2 Use Restrictions
+
+You shall use Двач/2ch - анонимный браузер in compliance with all applicable laws and not for any unlawful purpose.
+
+Use of Двач/2ch - анонимный браузер means that you have loaded, installed, or run Двач/2ch - анонимный браузер on a computer or similar device. If you install Двач/2ch - анонимный браузер onto a multi-user platform, server or network, each and every individual user of Двач/2ch - анонимный браузер must be licensed separately.
+
+4.3 Indemnity
+
+You will indemnify, hold harmless, and defend Ruslan Timchenko and Kirill Solovyov , its employees, agents and distributors against any and all claims, proceedings, demand and costs resulting from or in any way connected with your use of Ruslan Timchenko and Kirill Solovyov's Двач/2ch - анонимный браузер.
+
+4.4 Your Content
+
+The Services may contain information, text, links, graphics, photos, videos, or other materials (“Content”), including Content created with or submitted to the Services by you or through your Account (“Your Content”). We take no responsibility for and we do not expressly or implicitly endorse any of Your Content.
+
+By submitting Your Content to the Services, you represent and warrant that you have all rights, power, and authority necessary to grant the rights to Your Content contained within these Terms. Because you alone are responsible for Your Content, you may expose yourself to liability if you post or share Content without all necessary rights.
+
+You retain any ownership rights you have in Your Content, but you grant Двач/2ch - анонимный браузер the following license to use that Content:
+
+When Your Content is created with or submitted to the Services, you grant us a worldwide, royalty-free, perpetual, irrevocable, non-exclusive, transferable, and sublicensable license to use, copy, modify, adapt, prepare derivative works from, distribute, perform, and display Your Content and any name, username, voice, or likeness provided in connection with Your Content in all media formats and channels now known or later developed. This license includes the right for us to make Your Content available for syndication, broadcast, distribution, or publication by other companies, organizations, or individuals who partner with Двач/2ch - анонимный браузер. You also agree that we may remove metadata associated with Your Content, and you irrevocably waive any claims and assertions of moral rights or attribution with respect to Your Content.
+
+Any ideas, suggestions, and feedback about Двач/2ch - анонимный браузер or our Services that you provide to us are entirely voluntary, and you agree that Двач/2ch - анонимный браузер may use such ideas, suggestions, and feedback without compensation or obligation to you.
+
+Although we have no obligation to screen, edit, or monitor Your Content, we may, in our sole discretion, delete or remove Your Content at any time and for any reason, including for a violation of these Terms, a violation of our Content Policy, or if you otherwise create liability for us.
+
+4.5. Third-Party Content, Advertisements And Promotions
+
+The Services may contain links to third-party websites, products, or services, which may be posted by advertisers, our affiliates, our partners, or other users (“Third-Party Content”). Third-Party Content is not under our control, and we are not responsible for any of their websites, products, or services. Your use of Third-Party Content is at your own risk and you should make any investigation you feel necessary before proceeding with any transaction in connection with such Third-Party Content.
+
+The Services may also contain sponsored Third-Party Content or advertisements. The type, degree, and targeting of advertisements are subject to change, and you acknowledge and agree that we may place advertisements in connection with the display of any Content or information on the Services, including Your Content.
+
+If you choose to use the Services to conduct a promotion, including a contest or sweepstakes, you alone are responsible for conducting the promotion in compliance with all applicable laws and regulations. The terms of your promotion must specifically state that the promotion is not sponsored by, endorsed by, or associated with Двач/2ch - анонимный браузер and the rules for your promotion must require each entrant or participant to release Двач/2ch - анонимный браузер from any liability related to the promotion.
+
+4.6. Things You Cannot Do
+
+When accessing or using the Services, you must respect others and their rights, including by following these Terms and the Content Policy, so that we all may continue to use and enjoy the Services. We support the responsible reporting of security vulnerabilities.
+
+When accessing or using our Services, you will not:
+
+Create or submit Content that violates our Content Policy or attempt to circumvent any content-filtering techniques we use;
+Use the Services to violate applicable law or infringe any person or entity's intellectual property or any other proprietary rights;
+Attempt to gain unauthorized access to the Services (or to other computer systems or networks connected to or used together with the Services);
+Upload, transmit, or distribute to or through the Services any computer viruses, worms, or other software intended to interfere with the intended operation of a computer system or data;
+Use the Services to harvest, collect, gather or assemble information or data regarding the Services or users of the Services except as permitted in these Terms or in a separate agreement with Двач/2ch - анонимный браузер;
+Use the Services in any manner that could interfere with, disrupt, negatively affect, or inhibit other users from fully enjoying the Services or that could damage, disable, overburden, or impair the functioning of the Services in any manner;
+Intentionally negate any user's actions to delete or edit their Content on the Services; or
+Access, query, or search the Services with any automated system, other than through our published interfaces and pursuant to their applicable terms.
+
+4.7 Warranties
+
+Except as expressly stated in writing, Ruslan Timchenko and Kirill Solovyov makes no representation or warranties in respect of this software and expressly excludes all other warranties, expressed or implied, oral or written, including, without limitation, any implied warranties of merchantable quality or fitness for a particular purpose.
+
+4.8 Governing Law
+
+This Agreement shall be governed by the law of the Russian Federation applicable therein. You hereby irrevocably attorn and submit to the non-exclusive jurisdiction of the courts of Russian Federation therefrom. If any provision shall be considered unlawful, void or otherwise unenforceable, then that provision shall be deemed severable from this License and not affect the validity and enforceability of any other provisions.
+
+4.9 Termination
+
+Any failure to comply with the terms and conditions of this Agreement will result in automatic and immediate termination of this license. Upon termination of this license granted herein for any reason, you agree to immediately cease use of Двач/2ch - анонимный браузер and destroy all copies of Двач/2ch - анонимный браузер supplied under this Agreement. The financial obligations incurred by you shall survive the expiration or termination of this license.
+
+5. DISCLAIMER OF WARRANTY
+
+THIS SOFTWARE AND THE ACCOMPANYING FILES ARE SOLD "AS IS" AND WITHOUT WARRANTIES AS TO PERFORMANCE OR MERCHANTABILITY OR ANY OTHER WARRANTIES WHETHER EXPRESSED OR IMPLIED. THIS DISCLAIMER CONCERNS ALL FILES GENERATED AND EDITED BY Двач/2ch - анонимный браузер AS WELL.
+
+6. LIMITATION OF LIABILITY
+
+IN NO EVENT AND UNDER NO THEORY OF LIABILITY, INCLUDING CONTRACT, TORT, NEGLIGENCE, STRICT LIABILITY, WARRANTY, OR OTHERWISE, WILL THE ДВАЧ/2CH - АНОНИМНЫЙ БРАУЗЕР ENTITIES BE LIABLE TO YOU FOR ANY INDIRECT, CONSEQUENTIAL, EXEMPLARY, INCIDENTAL, SPECIAL, OR PUNITIVE DAMAGES, OR LOST PROFITS ARISING FROM OR RELATING TO THESE TERMS OR THE SERVICES, INCLUDING THOSE ARISING FROM OR RELATING TO CONTENT MADE AVAILABLE ON THE SERVICES THAT IS ALLEGED TO BE DEFAMATORY, OFFENSIVE, OR ILLEGAL. ACCESS TO, AND USE OF, THE SERVICES IS AT YOUR OWN DISCRETION AND RISK, AND YOU WILL BE SOLELY RESPONSIBLE FOR ANY DAMAGE TO YOUR DEVICE OR COMPUTER SYSTEM, OR LOSS OF DATA RESULTING THEREFROM. RUSLAN TIMCHENKO AND KIRILL SOLOVYOV'S ENTIRE LIABILITY, WITHOUT EXCEPTION, IS LIMITED TO THE CUSTOMERS' REIMBURSEMENT OF THE PURCHASE PRICE OF THE SOFTWARE (MAXIMUM BEING THE LESSER OF THE AMOUNT PAID BY YOU AND THE SUGGESTED RETAIL PRICE AS LISTED BY RUSLAN TIMCHENKO AND KIRILL SOLOVYOV ) IN EXCHANGE FOR THE RETURN OF THE PRODUCT, ALL COPIES, REGISTRATION PAPERS AND MANUALS, AND ALL MATERIALS THAT CONSTITUTE A TRANSFER OF LICENSE FROM THE CUSTOMER BACK TO RUSLAN TIMCHENKO AND KIRILL SOLOVYOV.
+
+7. CONSENT OF USE OF DATA
+
+You agree that Ruslan Timchenko and Kirill Solovyov may collect and use information gathered in any manner as part of the product support services provided to you, if any, related to Двач/2ch - анонимный браузер. Ruslan Timchenko and Kirill Solovyov may also use this information to provide notices to you which may be of use or interest to you.
 """
 }
 
